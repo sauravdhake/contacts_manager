49e73e18e60e4af0fc40c1063b3cb84e171018ff
sauravdhake <saurav.dhake.100@gmail.com>
2022-10-11 20:35:06 +0530

    initial_code added

---
+++ server/src/index.js
@@ -0,0 +1,5 @@
+// const app = require("./app");
+// const port = process.env.PORT || 8080;
+// const mongoose = require("mongoose");
+// mongoose.connect("mongodb+srv://BLOG_APP:8208431391@cluster0.k2u4lee.mongodb.net/BLOG_APP",()=>console.log("DB is connected"));
+// app.listen(port,()=>console.log(`Server is running at ${port}`))
---
+++ server/src/models/posts.js
@@ -0,0 +1,47 @@
+// const mongoose = require("mongoose");
+// const Schema = mongoose.Schema;
+
+// const postSchema = new Schema({
+
+
+
+//     name:{
+        
+        
+//         type:String,required:true
+    
+//     },
+
+
+//     location:{
+        
+//         type:String,required:true
+    
+//     },
+
+
+//     description:{
+        
+//         type:String,required:true
+    
+    
+//     },
+
+
+//     PostImage:{
+        
+//         type:String,required:true
+    
+//     },
+
+
+//     date:{
+//         type:Date,required:true
+    
+//     },
+//     likes:Number
+// });
+
+// const post = mongoose.model("posts",postSchema);
+
+// module.exports = post;
